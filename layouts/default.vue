6c43e9da963bc8e3545d22c219fc5197828d7f61
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-08-09 12:56:59 +0700

    update default styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,7 +28,9 @@ export default {
   --title-color: 50, 50, 93;
   --text-color: 107, 124, 147;
 }
+</style>
 
+<style>
 html {
   font-family: var(--font);
   font-size: 16px;
@@ -45,7 +47,9 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
+</style>
 
+<style>
 [row] {
   display: flex;
   flex-direction: row;
@@ -84,15 +88,15 @@ p {
   line-height: 26px;
 }
 
-.ma-0 { margin: 0rem; } .ml-0 { margin-left: 0rem; } .mr-0 { margin-right: 0rem; } .mt-0 { margin-top: 0rem; } .mb-0 { margin-bottom: 0rem; }
-.ma-1 { margin: 1rem; } .ml-1 { margin-left: 1rem; } .mr-1 { margin-right: 1rem; } .mt-1 { margin-top: 1rem; } .mb-1 { margin-bottom: 1rem; }
-.ma-2 { margin: 2rem; } .ml-2 { margin-left: 2rem; } .mr-2 { margin-right: 2rem; } .mt-2 { margin-top: 2rem; } .mb-2 { margin-bottom: 2rem; }
-.ma-3 { margin: 3rem; } .ml-3 { margin-left: 3rem; } .mr-3 { margin-right: 3rem; } .mt-3 { margin-top: 3rem; } .mb-3 { margin-bottom: 3rem; }
+.ma-0 { margin: 0rem!important; } .ml-0 { margin-left: 0rem!important; } .mr-0 { margin-right: 0rem!important; } .mt-0 { margin-top: 0rem!important; } .mb-0 { margin-bottom: 0rem!important; }
+.ma-1 { margin: 1rem!important; } .ml-1 { margin-left: 1rem!important; } .mr-1 { margin-right: 1rem!important; } .mt-1 { margin-top: 1rem!important; } .mb-1 { margin-bottom: 1rem!important; }
+.ma-2 { margin: 2rem!important; } .ml-2 { margin-left: 2rem!important; } .mr-2 { margin-right: 2rem!important; } .mt-2 { margin-top: 2rem!important; } .mb-2 { margin-bottom: 2rem!important; }
+.ma-3 { margin: 3rem!important; } .ml-3 { margin-left: 3rem!important; } .mr-3 { margin-right: 3rem!important; } .mt-3 { margin-top: 3rem!important; } .mb-3 { margin-bottom: 3rem!important; }
 
-.pa-0 { padding: 0rem; } .pl-0 { padding-left: 0rem; } .pr-0 { padding-right: 0rem; } .pt-0 { padding-top: 0rem; } .pb-0 { padding-bottom: 0rem; }
-.pa-1 { padding: 1rem; } .pl-1 { padding-left: 1rem; } .pr-1 { padding-right: 1rem; } .pt-1 { padding-top: 1rem; } .pb-1 { padding-bottom: 1rem; }
-.pa-2 { padding: 2rem; } .pl-2 { padding-left: 2rem; } .pr-2 { padding-right: 2rem; } .pt-2 { padding-top: 2rem; } .pb-2 { padding-bottom: 2rem; }
-.pa-3 { padding: 3rem; } .pl-3 { padding-left: 3rem; } .pr-3 { padding-right: 3rem; } .pt-3 { padding-top: 3rem; } .pb-3 { padding-bottom: 3rem; }
+.pa-0 { padding: 0rem!important; } .pl-0 { padding-left: 0rem!important; } .pr-0 { padding-right: 0rem!important; } .pt-0 { padding-top: 0rem!important; } .pb-0 { padding-bottom: 0rem!important; }
+.pa-1 { padding: 1rem!important; } .pl-1 { padding-left: 1rem!important; } .pr-1 { padding-right: 1rem!important; } .pt-1 { padding-top: 1rem!important; } .pb-1 { padding-bottom: 1rem!important; }
+.pa-2 { padding: 2rem!important; } .pl-2 { padding-left: 2rem!important; } .pr-2 { padding-right: 2rem!important; } .pt-2 { padding-top: 2rem!important; } .pb-2 { padding-bottom: 2rem!important; }
+.pa-3 { padding: 3rem!important; } .pl-3 { padding-left: 3rem!important; } .pr-3 { padding-right: 3rem!important; } .pt-3 { padding-top: 3rem!important; } .pb-3 { padding-bottom: 3rem!important; }
 
 .text--white { color: #fff!important; }
 .text--primary { color: rgb(var(--primary-color))!important; }
